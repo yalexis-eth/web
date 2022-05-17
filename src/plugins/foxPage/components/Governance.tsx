b2e3b05103cf3e7534f95a8997d3457b598ae21f
NeOMakinG <valentin.szczupak@prestashop.com>
2022-05-17 11:51:22 +0200

    fix: finish governance component

--- src/plugins/foxPage/components/Governance.tsx
+++ src/plugins/foxPage/components/Governance.tsx
@@ -1,12 +1,9 @@
-import { Box, Flex } from '@chakra-ui/layout'
-import { Button, Link, Text as CText, useColorModeValue } from '@chakra-ui/react'
+import { Badge, Box, Flex } from '@chakra-ui/layout'
+import { Link, Progress, Text as CText, useColorModeValue } from '@chakra-ui/react'
 import { useTranslate } from 'react-polyglot'
-import { fox } from 'test/mocks/assets'
 import { Amount } from 'components/Amount/Amount'
-import { AssetIcon } from 'components/AssetIcon'
 import { Card } from 'components/Card/Card'
 import { Text } from 'components/Text/Text'
-import { BigNumber } from 'lib/bignumber/bignumber'
 
 export const Governance = () => {
   const translate = useTranslate()
@@ -28,7 +25,61 @@ export const Governance = () => {
           color='gray.500'
         />
       </Card.Header>
-      <Card.Body></Card.Body>
+      <Card.Body>
+        <Box>
+          <Link
+            isExternal
+            href={'#'}
+            fontWeight='semibold'
+            color={linkColor}
+            mb={4}
+            display='inline-block'
+          >
+            <Text translation='[SCP-71] Proposal to Create the KeepKey Workstream' />
+          </Link>
+          <Box mb={4}>
+            <Flex justifyContent='space-between' mb={2}>
+              <Text translation='plugins.foxPage.for' fontWeight='semibold' />
+
+              <Flex>
+                <Text translation='2,934,283' fontWeight='semibold' mr={2} />
+                <Badge
+                  colorScheme='blue'
+                  display='flex'
+                  alignItems='center'
+                  px={2}
+                  py={'2px'}
+                  borderRadius='md'
+                >
+                  <Amount.Percent value={'0.6698'} fontWeight='normal' />
+                </Badge>
+              </Flex>
+            </Flex>
+            <Progress value={66.98} height={1} colorScheme='green' />
+          </Box>
+
+          <Box mb={4}>
+            <Flex justifyContent='space-between' mb={2}>
+              <Text translation='plugins.foxPage.against' fontWeight='semibold' />
+
+              <Flex>
+                <Text translation='1,446,828' fontWeight='semibold' mr={2} />
+                <Badge
+                  colorScheme='blue'
+                  display='flex'
+                  alignItems='center'
+                  px={2}
+                  py={'2px'}
+                  borderRadius='md'
+                >
+                  <Amount.Percent value={'0.3302'} fontWeight='normal' />
+                </Badge>
+              </Flex>
+            </Flex>
+            <Progress value={33.02} height={1} colorScheme='green' />
+          </Box>
+        </Box>
+      </Card.Body>
     </Card>
   )
 }
